0f3fb97f9f1878b56060362ce3cdfa4111420d6d
Tim Kelly <tim.kelly@4mation.com.au>
2021-08-13 14:20:29 +1000

    Completed Angular Tour Of Heroes

---
+++ src/app/message.service.ts
@@ -0,0 +1,17 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  messages: string[] = [];
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
+}
